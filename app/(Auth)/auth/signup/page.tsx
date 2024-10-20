a5098ac3f7d76be058e3236d351a6db6b4467680
Sina Pirani <sinapiranidev@gmail.com>
2024-10-20 20:16:01 +0330

    Feat: Add credentials include to

--- app/(Auth)/auth/signup/page.tsx
+++ app/(Auth)/auth/signup/page.tsx
@@ -73,6 +73,7 @@ export default function Signup() {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: 'include',
         body: JSON.stringify(values),
       }
     )
