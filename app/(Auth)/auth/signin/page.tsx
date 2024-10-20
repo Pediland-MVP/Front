c53935ce95f9cc25ac8cca66c2c99f2f5b8b6bef
Sina Pirani <sinapiranidev@gmail.com>
2024-10-20 20:08:49 +0330

    Credentials include for signin

--- app/(Auth)/auth/signin/page.tsx
+++ app/(Auth)/auth/signin/page.tsx
@@ -48,6 +48,7 @@ export default function Login() {
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: 'include',
       body: JSON.stringify(values),
     })
       .then(async res => {
