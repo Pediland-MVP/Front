c1d14915bd1a84d25ab34eea15e45ddb31abf066
Sina Pirani <sinapiranidev@gmail.com>
2024-11-08 18:46:56 +0330

    Feat: Product page is async and fectch is serverSide

--- app/(Console)/console/products/[id]/page.tsx
+++ app/(Console)/console/products/[id]/page.tsx
@@ -1,51 +1,24 @@
 'use client'
 
-import { toast } from "@/components/ui/use-toast"
-import { ProductNamespace } from "@/types/product"
-import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
 import ProductFormSkeleton from "../components/product.form.skeleton"
 import ProductForm from "../components/product.form"
 import InstaDirectUi from "@/components/global/instaDirectUi"
+import { redirect } from "next/navigation"
 
-export default function Page({params: {id}}: {params: {id: string}}) {
+export default async function Page({params}: {params: Promise<{id: string}>}) {
 
-    const [product, setProduct] = useState<ProductNamespace.Product>()
+    const { id } = await params 
 
-    useEffect(() => {
-        console.log(product);
-    }, [product])
-
-    const router = useRouter()
-
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_API_URL}/products/${id}`, {
-                    credentials: 'include'
-                })
-                if (!response.ok) {
-                    router.push(`/console/products`)
-                    toast({
-                        title: 'پیدا نشد',
-                        variant: 'destructive',
-                    })
-                }
-                const data = await response.json()
-                setProduct(data)
-            }
-            catch(e) {
-                router.push(`/console/products`)
-                toast({
-                    title: 'پیدا نشد',
-                    variant: 'destructive',
-                })
-            }
+    const product = await fetch(`${process.env.NEXT_PUBLIC_BACK_API_URL}/products/${id}`, {
+        credentials: 'include'
+    }).then(res => {
+        if (!res.ok) {
+            redirect(`/console/products`)
         }
-
-        fetchProduct()
-    }, [])
-    console.log(typeof product, !!product);
+        return res.json()
+    }).catch(e => {
+        redirect(`/console/products`)
+    })
     
 
     if (!product) {
