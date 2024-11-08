b515adf6fd95ed56352be280acc4b3b4fcd51f4b
Sina Pirani <sinapiranidev@gmail.com>
2024-11-08 22:36:25 +0330

    Feat: Split product to two component to await id params

--- app/(Console)/console/products/[id]/page.tsx
+++ app/(Console)/console/products/[id]/page.tsx
@@ -1,39 +1,12 @@
-'use client'
-
-import ProductFormSkeleton from "../components/product.form.skeleton"
-import ProductForm from "../components/product.form"
-import InstaDirectUi from "@/components/global/instaDirectUi"
-import { redirect } from "next/navigation"
+import Product from "./product"
 
 export default async function Page({params}: {params: Promise<{id: string}>}) {
 
     const { id } = await params 
-
-    const product = await fetch(`${process.env.NEXT_PUBLIC_BACK_API_URL}/products/${id}`, {
-        credentials: 'include'
-    }).then(res => {
-        if (!res.ok) {
-            redirect(`/console/products`)
-        }
-        return res.json()
-    }).catch(e => {
-        redirect(`/console/products`)
-    })
     
 
-    if (!product) {
-        return <ProductFormSkeleton/>
-    }
-    
     return (
-        <div className="h-full flex gap-4">
-        <div className="w-2/3 h-[calc(100vh-2rem)] bg-white shadow rounded-2xl p-4">
-          <ProductForm shouldBeEdit={product} />
-        </div>
-        <div className="w-1/3 h-[calc(100vh-2rem)] bg-white shadow rounded-2xl p-4">
-          <InstaDirectUi />
-        </div>
-      </div>
+        <Product id={id} />
     )
 
 }
---
+++ app/(Console)/console/products/[id]/product.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { toast } from "@/components/ui/use-toast"
+import { ProductNamespace } from "@/types/product"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import ProductFormSkeleton from "../components/product.form.skeleton"
+import ProductForm from "../components/product.form"
+import InstaDirectUi from "@/components/global/instaDirectUi"
+
+export default function Product({ id }: {id: string}) {
+
+    const [product, setProduct] = useState<ProductNamespace.Product>()
+
+    const router = useRouter()
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_BACK_API_URL}/products/${id}`, {
+                    credentials: 'include'
+                })
+                if (!response.ok) {
+                    router.push(`/console/products`)
+                    toast({
+                        title: 'پیدا نشد',
+                        variant: 'destructive',
+                    })
+                }
+                const data = await response.json()
+                setProduct(data)
+            }
+            catch(e) {
+                router.push(`/console/products`)
+                toast({
+                    title: 'پیدا نشد',
+                    variant: 'destructive',
+                })
+            }
+        }
+
+        fetchProduct()
+    }, [])
+    console.log(typeof product, !!product);
+    
+
+    if (!product) {
+        return <ProductFormSkeleton/>
+    }
+    
+    return (
+        <div className="h-full flex gap-4">
+        <div className="w-2/3 h-[calc(100vh-2rem)] bg-white shadow rounded-2xl p-4">
+          <ProductForm shouldBeEdit={product} />
+        </div>
+        <div className="w-1/3 h-[calc(100vh-2rem)] bg-white shadow rounded-2xl p-4">
+          <InstaDirectUi />
+        </div>
+      </div>
+    )
+
+}
