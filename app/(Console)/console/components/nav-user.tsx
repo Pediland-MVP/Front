5cab84882b532ff1944e382eb2e85a5495dae540
Sina Pirani <sinapiranidev@gmail.com>
2024-11-25 22:30:42 +0330

    Feat: Logout

--- app/(Console)/console/components/nav-user.tsx
+++ app/(Console)/console/components/nav-user.tsx
@@ -1,37 +1,18 @@
 "use client";
 
 import {
-  BadgeCheck,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   SidebarMenu,
-  SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
 } from "@/components/theme/ui/sidebar";
-import {
-  SealCheck,
-  Sparkle,
-  Wallet,
-  Bell,
-  SignOut,
-} from "@phosphor-icons/react/dist/ssr";
 import { useTranslations } from "next-intl";
+import { Button } from "@/components/ui/button";
+import { SignOut } from "@phosphor-icons/react/dist/ssr";
+import { toast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import logger from "@/app/utils/logger";
+import { useState } from "react";
+import LoadingSpinner from "@/components/ui/loadingSpinner";
 
 export function NavUser({
   user,
@@ -44,65 +25,50 @@ export function NavUser({
 }) {
   const { isMobile } = useSidebar();
 
-  const t = useTranslations('General')
+  const t = useTranslations("General");
+  const [isLogoutLoading, setIsLogoutLoading] = useState<boolean>(false)
+
+  const router = useRouter();
+  const logoutHandler = async () => {
+    setIsLogoutLoading(true)
+    await fetch("/api/logout", {
+      method: "DELETE",
+      credentials: "include",
+    })
+    .then(async (res) => {
+
+      logger.debug("res", await res.text())
+      if (!res.ok) {
+        toast({
+          title: t("logoutFailed"),
+          variant: "destructive",
+        });
+        return;
+      }
+      toast({
+        title: t("logoutSuccess"),
+      });
+      router.push("/");
+    })
+    .catch(e => {
+      toast({
+        title: t("logoutFailed"),
+        variant: "destructive",
+      });
+    })
+    .finally(() => {
+      setIsLogoutLoading(false)
+    })
+  };
 
   return (
     <SidebarMenu>
       <SidebarMenuItem>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-10 w-10 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-full bg-blue-100">
-                  پق
-                </AvatarFallback>
-              </Avatar>
-              <div className="grid flex-1 text-right text-sm leading-snug">
-                <span className="truncate font-semibold">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <ChevronsUpDown className="ml-auto size-4" />
-            </SidebarMenuButton>
-          </DropdownMenuTrigger>
-
-          <DropdownMenuContent
-            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
-            align="end"
-            sideOffset={4}
-          >
-            {/* <DropdownMenuGroup>
-              <DropdownMenuItem>
-                ارتقا حساب
-                <Sparkle size={24} />
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator /> */}
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                {t('profile')}
-                <SealCheck size={24} />
-              </DropdownMenuItem>
-              {/* <DropdownMenuItem>
-                مالی
-                <Wallet size={24} />
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                نوتیفیکیشن
-                <Bell size={24} />
-              </DropdownMenuItem> */}
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              {t('logout')}
-              <SignOut size={24} />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Button onClick={logoutHandler} size={"icon"} variant={"outline"}>
+          {
+            isLogoutLoading ? <LoadingSpinner/> : <SignOut size={24} />
+          }
+        </Button>
       </SidebarMenuItem>
     </SidebarMenu>
   );
---
+++ app/api/logout/route.ts
@@ -0,0 +1,11 @@
+import logger from "@/app/utils/logger";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function DELETE(request: NextRequest) {
+
+    const response = NextResponse.json({status: 200});
+    logger.debug("Cookies", response.cookies.getAll())
+    response.cookies.delete('token')
+    return response
+
+}
