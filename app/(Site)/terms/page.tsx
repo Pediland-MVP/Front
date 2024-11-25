7b5185dcfca21153ae48b1faa13c5344cd7a71b1
pediland <pediland.com@gmail.com>
2024-11-25 18:58:18 +0100

    - Privacy n Terms Pages and Links

--- app/(Site)/terms/page.tsx
+++ app/(Site)/terms/page.tsx
@@ -1,71 +1,62 @@
-"use client"
+"use client";
 
-import { useTranslations } from 'next-intl';
-import React from 'react';
+import { useTranslations } from "next-intl";
+import React from "react";
 
 export default function TermsPage() {
-  const t = useTranslations('TermsAndConditions');
+  const t = useTranslations("TermsAndConditions");
 
   return (
-    <div className='w-full max-w-[72rem] flex flex-col mx-auto md:mt-[4rem] mt-6'>
-      <div className="px-8 md:py-8 mt-8 bg-white rounded-lg">      
-        <h1 className="text-4xl font-semibold -mb-2">{t('title')}</h1>
+    <div className="w-full max-w-[72rem] flex flex-col mx-auto md:mt-[4rem] mt-6">
+      <div className="px-8 md:py-8 mt-8 bg-white rounded-lg">
+        <h1 className="text-4xl font-semibold -mb-2">{t("title")}</h1>
         <section className="text-lg leading-7">
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('introduction.title')}</h3>
-          <p className="mb-4">{t('introduction.paragraph1')}</p>
-          <p className="mb-4">{t('introduction.paragraph2')}</p>
-          <p className="mb-4">{t('introduction.paragraph3')}</p>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("introduction.title")}
+          </h3>
+          <p className="mb-4">{t("introduction.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('generalRules.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 11 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`generalRules.rule${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.0.title")}
+          </h3>
+          <p className="mb-4">{t("sections.0.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('registrationConditions.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 7 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`registrationConditions.condition${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.1.title")}
+          </h3>
+          <p className="mb-4">{t("sections.1.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('informationChange.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 2 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`informationChange.point${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.2.title")}
+          </h3>
+          <p className="mb-4">{t("sections.2.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('confidentiality.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 2 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`confidentiality.point${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.3.title")}
+          </h3>
+          <p className="mb-4">{t("sections.3.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('paymentAndRefund.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 2 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`paymentAndRefund.point${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.4.title")}
+          </h3>
+          <p className="mb-4">{t("sections.4.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('otherCases.title')}</h3>
-          <ul className="list-disc pl-5 mb-4 px-10">
-            {Array.from({ length: 5 }, (_, i) => (
-              <li key={i} className="mb-2">{t(`otherCases.point${i + 1}`)}</li>
-            ))}
-          </ul>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.5.title")}
+          </h3>
+          <p className="mb-4">{t("sections.5.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('forceMajeure.title')}</h3>
-          <p className="mb-4">{t('forceMajeure.description')}</p>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("sections.6.title")}
+          </h3>
+          <p className="mb-4">{t("sections.6.paragraph1")}</p>
 
-          <h3 className="text-3xl font-semibold mb-4 mt-12">{t('disputeResolution.title')}</h3>
-          <p>{t('disputeResolution.description')}</p>
+          <h3 className="text-3xl font-semibold mb-4 mt-12">
+            {t("disclaimer.title")}
+          </h3>
+          <p className="mb-4">{t("disclaimer.paragraph1")}</p>
         </section>
       </div>
     </div>
   );
 }
-
