40d1f6bebc4101dc99c99644673274a4a6ac097b
Sina Pirani <sinapiranidev@gmail.com>
2024-12-23 22:00:10 +0330

    Optimize JWT verification by re-enabling secret check and adding timing logs.

--- middleware.ts
+++ middleware.ts
@@ -89,10 +89,12 @@ async function authMiddleware(request: NextRequest) {
 
 async function parseJwt(token: string, request: NextRequest) {
   try {
-    // const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-    // const jwt = await jose.jwtVerify(token, secret);
-    // return jwt;
-    return !!token ? {payload: {isVerified: true}} : false
+    console.time("jwtVerify")
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const jwt = await jose.jwtVerify(token, secret);
+    console.timeEnd('jwtVerify')
+    return jwt;
+    // return !!token ? {payload: {isVerified: true}} : false
   } catch (error) {
     return false;
   }
