392da16648cee840e58f5602dd7cb6e3ae309d81
Sina Pirani <sinapiranidev@gmail.com>
2025-01-17 04:34:44 +0330

    Debug: Removed unneccesary form

--- app/(Shop)/[shopId]/[productId]/orders/components/customerDetails.tsx
+++ app/(Shop)/[shopId]/[productId]/orders/components/customerDetails.tsx
@@ -8,13 +8,17 @@ import { zodResolver } from "@hookform/resolvers/zod";
 // UI
 import { Input } from "@/components/theme/ui/input";
 import { UserRectangle } from "@phosphor-icons/react/dist/ssr";
-import { FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form";
-import LoadingButton from '@/components/ui/button-loading';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import LoadingButton from "@/components/ui/button-loading";
 import useOrder from "../hooks/useOrder";
 
 export default function CustomerDetails() {
   const t = useTranslations("Checkout");
-
 
   const {
     register,
@@ -26,11 +30,11 @@ export default function CustomerDetails() {
     resolver: zodResolver(orderFormSchema),
   });
 
-  const { createOrder, loading: isCreateOrderLoading } = useOrder()
+  const { createOrder, loading: isCreateOrderLoading } = useOrder();
 
   const createOrderHandler = async () => {
-    createOrder()
-  }
+    createOrder();
+  };
 
   return (
     <div className="_customer-details p-3">
@@ -38,63 +42,72 @@ export default function CustomerDetails() {
         <UserRectangle size={28} weight="duotone" className="text-primary" />
         {t("customerDetails")}
       </h2>
-      <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit((data) => console.log(data))}>
-          <div className="grid gap-2">
-            <FormField
-              control={control}
-              name="firstname"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("firstName")}</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="firstname"
-                      {...register("firstname", { required: true })} />
-                  </FormControl>
-                  {errors.firstname && (
-                    <span className="text-red-500 text-sm">{t("required")}</span>
-                  )}
-                </FormItem>
-              )}
-            />
 
-            <FormField
-              control={control}
-              name="lastname"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("lastName")}</FormLabel>
-                  <FormControl>
-                    <Input id="lastname" {...register("lastname", { required: true })} />
-                  </FormControl>
-                  {errors.lastname && (
-                    <span className="text-red-500 text-sm">{t("required")}</span>
-                  )}
-                </FormItem>
+      <div className="grid gap-2">
+        <FormField
+          control={control}
+          name="firstname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("firstName")}</FormLabel>
+              <FormControl>
+                <Input
+                  id="firstname"
+                  {...register("firstname", { required: true })}
+                />
+              </FormControl>
+              {errors.firstname && (
+                <span className="text-red-500 text-sm">{t("required")}</span>
               )}
-            />
+            </FormItem>
+          )}
+        />
 
-            <FormField
-              control={control}
-              name="mobile"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("mobile")}</FormLabel>
-                  <FormControl>
-                    <Input id="mobile" {...register("mobile", { required: true })} />
-                  </FormControl>
-                  {errors.mobile && (
-                    <span className="text-red-500 text-sm">{t("required")}</span>
-                  )}
-                </FormItem>
+        <FormField
+          control={control}
+          name="lastname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("lastName")}</FormLabel>
+              <FormControl>
+                <Input
+                  id="lastname"
+                  {...register("lastname", { required: true })}
+                />
+              </FormControl>
+              {errors.lastname && (
+                <span className="text-red-500 text-sm">{t("required")}</span>
               )}
-            />
-          </div>
-        </form>
-      </FormProvider>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={control}
+          name="mobile"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>{t("mobile")}</FormLabel>
+              <FormControl>
+                <Input
+                  id="mobile"
+                  {...register("mobile", { required: true })}
+                />
+              </FormControl>
+              {errors.mobile && (
+                <span className="text-red-500 text-sm">{t("required")}</span>
+              )}
+            </FormItem>
+          )}
+        />
+      </div>
       <div className="mt-6 w-full">
-        <LoadingButton onClick={createOrderHandler} isLoading={isCreateOrderLoading} className="w-full" variant={"success"}>
+        <LoadingButton
+          onClick={createOrderHandler}
+          isLoading={isCreateOrderLoading}
+          className="w-full"
+          variant={"success"}
+        >
           {t("nextStep")}
         </LoadingButton>
       </div>
