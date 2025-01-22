88bc3c0447a0d5c83061799508f34c9f9f4204cc
Sina Pirani <sinapiranidev@gmail.com>
2025-01-22 22:22:37 +0330

    Feat: Order payment status pages

--- app/(Shop)/[shopId]/[productId]/order/components/order.processing.tsx
+++ app/(Shop)/[shopId]/[productId]/order/components/order.processing.tsx
@@ -1,12 +1,21 @@
 import { useTranslations } from "next-intl"
+import Image from "next/image";
 
 
 export default function OrderProcessing() {
     const t = useTranslations('Checkout');
     return (
-        <div className="_checkout flex flex-col justify-center items-center bg-white border h-[200px] rounded-xl p-5 md:p-10">
-            <p className="text-xl font-medium">{t('orderProcessing')}</p>
-            <p className="text-lg text-center">{t('orderProcessingDescription')}</p>
+        <div className="_checkout h-svh flex flex-col justify-center items-center bg-white border rounded-xl p-5 md:p-10">
+          <Image
+            src={"/images/emojies/smiling-face-with-hearts.webp"}
+            height={200}
+            width={200}
+            alt={"قلب"}
+          />
+          <p className="text-xl font-medium">{t("orderProcessing")}</p>
+          <p className="text-lg text-center">
+            {t("orderProcessingDescription")}
+          </p>
         </div>
     )
 
