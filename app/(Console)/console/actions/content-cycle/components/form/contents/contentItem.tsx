0e37fd771300320f845d572b235d0f975764087f
Sina Pirani <sinapiranidev@gmail.com>
2025-02-19 13:40:49 +0330

    Feat: Move all medias in one tab

--- app/(Console)/console/actions/content-cycle/components/form/contents/contentItem.tsx
+++ app/(Console)/console/actions/content-cycle/components/form/contents/contentItem.tsx
@@ -21,9 +21,7 @@ import {
   ArrowsOutCardinal,
   Chat,
   InstagramLogo,
-  Waveform,
-  Video,
-  Image,
+  Paperclip,
   Storefront,
 } from "@phosphor-icons/react/dist/ssr";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -34,7 +32,10 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { ContentCycleContentModeEnum, ContentCycleContentTypesEnum } from "@/app/constants/contentCycleContent.enum";
+import {
+  ContentCycleContentModeEnum,
+  ContentCycleContentTypesEnum,
+} from "@/app/constants/contentCycleContent.enum";
 import { Button } from "@/components/theme/ui/button";
 import { FileUploader } from "@/components/theme/ui/fileUploader";
 import { UploadedFile } from "@/components/theme/types/fileUploader";
@@ -46,7 +47,6 @@ import { useContentsUploaderContext } from "./useContentsUploaderContext";
 import { useContentsContext } from "./useContentsContext";
 import InstagramPostsDialog from "../../../../components/instagramPosts.dialog";
 import Catalogue from "../catalogue";
-import { v4 as UUID } from 'uuid'
 
 type MessageByTypeProps = {
   index: number;
@@ -58,17 +58,21 @@ export function MessageByType({ index, type, mode }: MessageByTypeProps) {
   const { files, setFiles } = useContentsUploaderContext();
   const t_ec = useTranslations("ERROR_CODES");
   const t_err = useTranslations("Automations.Errors");
-  const t_fileUploader = useTranslations('FileUploader')
+  const t_fileUploader = useTranslations("FileUploader");
 
   const {
     control,
     setValue,
+    getValues,
     formState: { errors },
   } = useFormContext<z.infer<typeof contentCycleFormSchema>>();
 
   const onChange = (files: UploadedFile[]) => {
     if (files.length === 0) {
-      setValue(`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.${index}.file`, null);
+      setValue(
+        `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}.file`,
+        null
+      );
     }
     if ("file" in files[0]) {
       setFiles((files) => {
@@ -77,22 +81,26 @@ export function MessageByType({ index, type, mode }: MessageByTypeProps) {
       const formData = new FormData();
       formData.append("file", files[0].file);
       const res = axios
-        .post(`${process.env.NEXT_PUBLIC_BACK_API_URL}/contentCycle/upload`, formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-          withCredentials: true,
-          onUploadProgress: (progressEvent) => {
-            if (progressEvent.total) {
-              const process = Math.round(
-                (progressEvent.loaded / progressEvent.total) * 100
-              );
-              setFiles((prev) => {
-                return [{ ...prev[0], process: process }];
-              });
-            }
-          },
-        })
+        .post(
+          `${process.env.NEXT_PUBLIC_BACK_API_URL}/contentCycle/upload`,
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            withCredentials: true,
+            onUploadProgress: (progressEvent) => {
+              if (progressEvent.total) {
+                const process = Math.round(
+                  (progressEvent.loaded / progressEvent.total) * 100
+                );
+                setFiles((prev) => {
+                  return [{ ...prev[0], process: process }];
+                });
+              }
+            },
+          }
+        )
         .then((res: AxiosResponse<FileNamespace.File>) => {
           setFiles([
             {
@@ -102,32 +110,45 @@ export function MessageByType({ index, type, mode }: MessageByTypeProps) {
             },
           ]);
 
-          setValue(`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.${index}.file`, {
-            id: res.data.id,
-            url: res.data.url,
-            mimeType: res.data.mimeType,
-          });
+          setValue(
+            `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}.file`,
+            {
+              id: res.data.id,
+              url: res.data.url,
+              mimeType: res.data.mimeType,
+            }
+          );
+          setValue(
+            `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}`,
+            {
+              ...getValues(
+                `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}`
+              ),
+              type: res.data.mimeType.split(
+                "/"
+              )[0] as ContentCycleContentTypesEnum,
+            }
+          );
         })
         .catch((err: AxiosError) => {
           const errorCode = t_ec(
             (err.response?.data as ExceptionMessage)?.code
           );
-          if (errorCode !== 'ERROR_CODES') {
+          if (errorCode !== "ERROR_CODES") {
             toast({
               title: errorCode,
               variant: "destructive",
             });
-            return
+            return;
           }
 
           if (err.status === 400) {
             toast({
               title: `${t_fileUploader(`Limits.${type}.text`)}. ${t_fileUploader(`Limits.${type}.formats`)}`,
               description: "لطفا یک فایل دیگر انتخاب کنید",
-              variant: "destructive"
-            })
+              variant: "destructive",
+            });
           }
-
         })
         .finally(() => {
           setFiles((prev) => {
@@ -138,30 +159,33 @@ export function MessageByType({ index, type, mode }: MessageByTypeProps) {
   };
 
   const t = useTranslations("Automations.Contents");
-  const { updateContents, contents } = useContentsContext()
+  const { updateContents, contents } = useContentsContext();
 
   switch (type) {
     case ContentCycleContentTypesEnum.INSTAGRAM_POST:
       return (
         <div className="relative flex justify-center items-center">
-          <InstagramPostsDialog
-            mode={mode}
-            index={index}
-          />
+          <InstagramPostsDialog mode={mode} index={index} />
         </div>
       );
     case ContentCycleContentTypesEnum.TEXT:
       return (
         <Controller
-          name={`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.${index}.text`}
+          name={`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}.text`}
           control={control}
           render={({ field, fieldState: { error } }) => (
             <FormItem>
-              <Textarea rows={5} placeholder={t("enterYourMessage")} {...field} />
+              <Textarea
+                rows={5}
+                placeholder={t("enterYourMessage")}
+                {...field}
+              />
               {error && (
                 <FormMessage>
                   {" "}
-                  {t_err(`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.text.${error.message}`)}{" "}
+                  {t_err(
+                    `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.text.${error.message}`
+                  )}{" "}
                 </FormMessage>
               )}
             </FormItem>
@@ -169,81 +193,40 @@ export function MessageByType({ index, type, mode }: MessageByTypeProps) {
         />
       );
 
-    case ContentCycleContentTypesEnum.AUDIO:
-      return (
-        <>
-          <FileUploader
-            multiple={false}
-            files={files}
-            setFiles={setFiles}
-            onChange={onChange}
-            accept="audio/*"
-          />
-          {errors.contents?.[index]?.file && (
-            <FormMessage>
-              {" "}
-              {t_err(
-                `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.audio.${errors.contents?.[index]?.file.message}`
-              )}{" "}
-            </FormMessage>
-          )}
-        </>
-      );
-
-    case ContentCycleContentTypesEnum.VIDEO:
-      return (
-        <>
-          <FileUploader
-            multiple={false}
-            files={files}
-            setFiles={setFiles}
-            onChange={onChange}
-            accept="video/*"
-          />
-          {errors.contents?.[index]?.file && (
-            <FormMessage>
-              {" "}
-              {t_err(
-                `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.video.${errors.contents?.[index]?.file.message}`
-              )}{" "}
-            </FormMessage>
-          )}
-        </>
-      );
-    case ContentCycleContentTypesEnum.IMAGE:
-      return (
-        <>
-          <FileUploader
-            multiple={false}
-            files={files}
-            setFiles={setFiles}
-            onChange={onChange}
-            accept="image/*"
-          />
-          {errors.contents?.[index]?.file && (
-            <FormMessage>
-              {" "}
-              {t_err(
-                `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.image.${errors.contents?.[index]?.file.message}`
-              )}{" "}
-            </FormMessage>
-          )}
-        </>
-      );
-
     case ContentCycleContentTypesEnum.PRODUCT:
+      return <Catalogue mode={mode} index={index} />;
+
+    default:
       return (
-        <Catalogue mode={mode} index={index}/>
-      )
+        <>
+          <FileUploader
+            multiple={false}
+            files={files}
+            setFiles={setFiles}
+            onChange={onChange}
+            accept="audio/*,video/*,image/*"
+          />
+          {errors.contents?.[index]?.file && (
+            <FormMessage>
+              {" "}
+              {t_err(
+                `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.media.${errors.contents?.[index]?.file.message}`
+              )}{" "}
+            </FormMessage>
+          )}
+        </>
+      );
   }
 }
 
 interface MessageTypeOption {
-  value: ContentCycleContentTypesEnum;
+  value: ContentCycleContentTypesEnum | "media";
   label: string;
   icon: React.ReactNode;
 }
 
+//FIXME: Should be refactored
+//BUG: Dont change my order!
 const messageTypeOptions: MessageTypeOption[] = [
   {
     value: ContentCycleContentTypesEnum.TEXT,
@@ -257,23 +240,13 @@ const messageTypeOptions: MessageTypeOption[] = [
   },
   {
     value: ContentCycleContentTypesEnum.PRODUCT,
-    label: 'Product',
-    icon: <Storefront className="h-6 w-6" />
+    label: "Product",
+    icon: <Storefront className="h-6 w-6" />,
   },
   {
-    value: ContentCycleContentTypesEnum.AUDIO,
-    label: "Audio",
-    icon: <Waveform className="h-6 w-6" />,
-  },
-  {
-    value: ContentCycleContentTypesEnum.VIDEO,
-    label: "Video",
-    icon: <Video className="h-6 w-6" />,
-  },
-  {
-    value: ContentCycleContentTypesEnum.IMAGE,
-    label: "Image",
-    icon: <Image className="h-6 w-6" />,
+    value: "media",
+    label: "Media",
+    icon: <Paperclip className="h-6 w-6" />,
   },
 ];
 
@@ -315,14 +288,18 @@ export default function ContentItem({
     // if the index is 1, set the haveConsent to false because for consent we need at least 2 item
     if (index === 1) {
       updateContents(0, {
-        ...mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? getValues().contents?.[0] : getValues().reminders?.[0],
+        ...(mode === ContentCycleContentModeEnum.CONTENT_CYCLE
+          ? getValues().contents?.[0]
+          : getValues().reminders?.[0]),
         haveConsent: false,
       });
     }
     trigger();
   };
 
-  const handleMessageTypeChange = (type: ContentCycleContentTypesEnum) => {
+  const handleMessageTypeChange = (
+    type: ContentCycleContentTypesEnum | "media"
+  ) => {
     // Create a new content object with the selected type
     const updatedContent = {
       ...contents[index],
@@ -334,7 +311,7 @@ export default function ContentItem({
         file: null,
       }),
       ...(type === ContentCycleContentTypesEnum.PRODUCT && {
-        products: [{}]
+        products: [{}],
       }),
     };
 
@@ -342,7 +319,9 @@ export default function ContentItem({
     updateContents(index, updatedContent);
 
     // Trigger form validation
-    trigger(`${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? 'contents' : 'reminders'}.${index}`);
+    trigger(
+      `${mode === ContentCycleContentModeEnum.CONTENT_CYCLE ? "contents" : "reminders"}.${index}`
+    );
   };
 
   return (
@@ -365,16 +344,17 @@ export default function ContentItem({
         </div>
       </div>
 
-      <div className="w-full grid grid-cols-6 gap-x-2 shrink-0 items-center">
+      <div className="w-full grid grid-cols-4 gap-x-2 shrink-0 items-center">
+        {/**FIXME: Should be refactor */}
         {messageTypeOptions.map((option) => (
           <Button
             key={option.value}
             type="button"
             variant={
-              contents[index].type === option.value ? "default" : "outline"
+              ((option.value === 'media' && ["image", "video", "audio"].includes(contents[index].type)) || contents[index].type === option.value) ? "default" : "outline"
             }
             className={`h-15 flex flex-col items-center justify-cente ${
-              contents[index].type === option.value ? "ring-2 ring-primary" : ""
+              ((option.value === 'media' && ["image", "video", "audio"].includes(contents[index].type)) || contents[index].type === option.value) ? "ring-2 ring-primary" : ""
             }`}
             onClick={() => handleMessageTypeChange(option.value)}
           >
@@ -386,51 +366,82 @@ export default function ContentItem({
 
       <div className="_content gap-3 flex flex-col w-full">
         <div className="flex flex-col gap-2 w-full">
-          <MessageByType mode={mode} index={index} type={contents[index].type} />
+          <MessageByType
+            mode={mode}
+            index={index}
+            type={contents[index].type}
+          />
 
-        {
-          contents[index].type === ContentCycleContentTypesEnum.TEXT && mode === ContentCycleContentModeEnum.CONTENT_CYCLE && (
-            <FormField
-              name={`contents.${index}.haveConsent`}
-              control={control}
-              render={({ field }) => (
-                <FormItem className="flex flex-col justify-start gap-y-2">
-                  <div className="flex items-center gap-x-2">
-                    <FormControl>
-                      <TooltipProvider>
-                        <Tooltip {...((contents.length > 1 && contents[index].type === ContentCycleContentTypesEnum.TEXT) && { open: false })}>
-                          <TooltipTrigger asChild disabled={contents.length > 1 || contents[index].type !== ContentCycleContentTypesEnum.TEXT}>
-                            <Checkbox
-                              disabled={contents.length <= 1 || contents[index].type !== ContentCycleContentTypesEnum.TEXT}
-                              dir="ltr"
-                              checked={field.value || false}
-                              onCheckedChange={field.onChange}
+          {contents[index].type === ContentCycleContentTypesEnum.TEXT &&
+            mode === ContentCycleContentModeEnum.CONTENT_CYCLE && (
+              <FormField
+                name={`contents.${index}.haveConsent`}
+                control={control}
+                render={({ field }) => (
+                  <FormItem className="flex flex-col justify-start gap-y-2">
+                    <div className="flex items-center gap-x-2">
+                      <FormControl>
+                        <TooltipProvider>
+                          <Tooltip
+                            {...(contents.length > 1 &&
+                              contents[index].type ===
+                                ContentCycleContentTypesEnum.TEXT && {
+                                open: false,
+                              })}
+                          >
+                            <TooltipTrigger
+                              asChild
+                              disabled={
+                                contents.length > 1 ||
+                                contents[index].type !==
+                                  ContentCycleContentTypesEnum.TEXT
+                              }
+                            >
+                              <Checkbox
+                                disabled={
+                                  contents.length <= 1 ||
+                                  contents[index].type !==
+                                    ContentCycleContentTypesEnum.TEXT
+                                }
+                                dir="ltr"
+                                checked={field.value || false}
+                                onCheckedChange={field.onChange}
+                              />
+                            </TooltipTrigger>
+                            <TooltipContent>
+                              {contents.length <= 1
+                                ? t("consentTooltip")
+                                : contents[index].type !==
+                                    ContentCycleContentTypesEnum.TEXT &&
+                                  t("consentTooltipType")}
+                            </TooltipContent>
+                          </Tooltip>
+                        </TooltipProvider>
+                      </FormControl>
+                      <FormLabel className="">{t("consent")}</FormLabel>
+                    </div>
+                    {!!field.value && (
+                      <Controller
+                        name={`contents.${index}.consentText`}
+                        control={control}
+                        render={({ field, fieldState: { error } }) => (
+                          <FormItem>
+                            <Input
+                              placeholder={t("consentMessage")}
+                              {...field}
                             />
-                          </TooltipTrigger>
-                          <TooltipContent>{contents.length <= 1 ? t("consentTooltip") : (contents[index].type !== ContentCycleContentTypesEnum.TEXT && t("consentTooltipType"))}</TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    </FormControl>
-                    <FormLabel className="">{t("consent")}</FormLabel>
-                  </div>
-                  {!!field.value && (
-                    <Controller
-                      name={`contents.${index}.consentText`}
-                      control={control}
-                      render={({ field, fieldState: { error } }) => (
-                        <FormItem>
-                          <Input placeholder={t("consentMessage")} {...field} />
-                          {error && <FormMessage> {error.message} </FormMessage>}
-                        </FormItem>
-                      )}
-                    />
-                  )}
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          )
-        }
+                            {error && (
+                              <FormMessage> {error.message} </FormMessage>
+                            )}
+                          </FormItem>
+                        )}
+                      />
+                    )}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )}
         </div>
       </div>
     </div>
