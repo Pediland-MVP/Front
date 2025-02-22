d3749175c9f8eeac9616a1d52a052d83d5bc222f
Sina Pirani <sinapiranidev@gmail.com>
2025-02-22 18:43:55 +0330

    Feat: userstate Condition

--- app/(Console)/console/settings/accounts/page.tsx
+++ app/(Console)/console/settings/accounts/page.tsx
@@ -22,45 +22,45 @@ export default function AccountPage() {
   const { hasSubscription, hasInstagram, isLoading, error } = useUser();
 
   if (isLoading) {
-    return <></>
-  }
-
-  if (hasSubscription && !hasInstagram) {
-    return <Button>سلام</Button>
-  }
-
-  if (!hasSubscription && !hasInstagram) {
-    return <p>تست</p>
+    return <></>;
   }
 
   return (
     <div className="_accounts-page flex h-full">
       <div className="sm:w-3/5 h-full">
         <Card className="border-l-2 border-gray-100 h-full p-6">
-          <div className="mb-6">
-            <h2 className="font-semibold text-primary mb-1">{t("title")}</h2>
-            <p className="text-[15px] text-muted-foreground">
-              {t("description")}
-            </p>
-          </div>
+          {(hasSubscription ? (
+            <>
+              <div className="mb-6">
+                <h2 className="font-semibold text-primary mb-1">
+                  {t("title")}
+                </h2>
+                <p className="text-[15px] text-muted-foreground">
+                  {t("description")}
+                </p>
+              </div>
 
-          <Suspense>
-            <Accounts
-              filteredInstagramPages={filteredInstagramPages}
-              setFilteredInstagramPages={setfilteredInstagramPages}
-            />
-          </Suspense>
+              <Suspense>
+                <Accounts
+                  filteredInstagramPages={filteredInstagramPages}
+                  setFilteredInstagramPages={setfilteredInstagramPages}
+                />
+              </Suspense>
 
-          {!hasInstagram && (
-            <Link
-              className="flex mt-6"
-              href={`${process.env.NEXT_PUBLIC_BACK_API_URL}/instagram/connectIG`}
-            >
-              <Button size={"sm"} className="w-full" variant={"success"}>
-                {t("add")}
-              </Button>
-            </Link>
-          )}
+              {!hasInstagram && (
+                <Link
+                  className="flex mt-6"
+                  href={`${process.env.NEXT_PUBLIC_BACK_API_URL}/instagram/connectIG`}
+                >
+                  <Button size={"sm"} className="w-full" variant={"success"}>
+                    {t("add")}
+                  </Button>
+                </Link>
+              )}
+            </>
+          ) : (
+            !hasSubscription && !hasInstagram
+          )) && <>تست</>}
         </Card>
       </div>
     </div>
