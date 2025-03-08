7406fe2095ecdf7384580242e1a7a017a26fe290
Sina Pirani <sinapiranidev@gmail.com>
2025-03-08 03:30:26 +0330

    Feat: Refreshtoken for WS

--- app/utils/socket.ts
+++ app/utils/socket.ts
@@ -1,11 +1,49 @@
+import api, { getAccessToken } from '@/hooks/swr/api-client';
 import { io } from 'socket.io-client';
 
 
-
-export const messagesSocket = io(process.env.NEXT_PUBLIC_WS_MESSAGE, {
+export let messagesSocket = io(process.env.NEXT_PUBLIC_WS_MESSAGE, {
     withCredentials: true,
-});
+    extraHeaders: {
+        Authorization: `Bearer ${getAccessToken()}`
+    }
+})
 
-export const commentsSocket = io(process.env.NEXT_PUBLIC_WS_COMMENTS, {
+messagesSocket.on('exception', async (data) => {
+    if (data.code === 401) {
+        await api.get('/users/me').then(() => {
+            messagesSocket.disconnect();
+            messagesSocket = io(process.env.NEXT_PUBLIC_WS_MESSAGE, {
+                withCredentials: true,
+                extraHeaders: {
+                    Authorization: `Bearer ${getAccessToken()}`
+                }
+            })
+            messagesSocket.connect();
+        })
+    }
+})
+
+
+
+export let commentsSocket = io(process.env.NEXT_PUBLIC_WS_COMMENTS, {
     withCredentials: true,
-});
+    extraHeaders: {
+        Authorization: `Bearer ${getAccessToken()}`
+    }
+})
+
+commentsSocket.on('exception', async (data) => {
+    if (data.code === 401) {
+        await api.get('/users/me').then(() => {
+            commentsSocket.disconnect();
+            commentsSocket = io(process.env.NEXT_PUBLIC_WS_COMMENTS, {
+                withCredentials: true,
+                extraHeaders: {
+                    Authorization: `Bearer ${getAccessToken()}`
+                }
+            })
+            commentsSocket.connect();
+        })
+    }
+})
