d84c89929a7fb7195bec5154b3a275b6c7a8d5f4
Sina Pirani <sinapiranidev@gmail.com>
2025-03-22 10:20:00 +0330

    Feat: Excelexport orders form

--- types/responseMessage.ts
+++ types/responseMessage.ts
@@ -1,6 +1,6 @@
 export type RES_CODES = 'PAID_FREE' | 'PAID_BY_GATEWAY' | 'PAYMENT_LINK_GENERATED' | 'ORDER_CREATED' | 'CONTACT_UPDATED'
 
-export interface IResponseMessage<T> {
+export interface IResponseMessage<T = undefined> {
     message: 'Created' | 'OK' | 'Updated' | string&{},
     statusCode: 200 | 201 | number&{},
     code: RES_CODES,
