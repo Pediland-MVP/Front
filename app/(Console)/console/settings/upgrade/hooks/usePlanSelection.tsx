3355282200ec06621395bcec608dea676c0a73e5
Sina Pirani <sinapiranidev@gmail.com>
2025-03-30 18:40:29 +0330

    Feat: Get plans just when authenticated

--- app/(Console)/console/settings/upgrade/hooks/usePlanSelection.tsx
+++ app/(Console)/console/settings/upgrade/hooks/usePlanSelection.tsx
@@ -1,3 +1,4 @@
+import useUser from "@/hooks/useUser"
 import { PlanNamespace } from "@/types/plans/plan.namespace"
 import { useEffect, useState } from "react"
 import useSWRImmutable from "swr/immutable"
@@ -6,7 +7,8 @@ export function usePlanSelection() {
   const [selectedPlan, setSelectedPlan] = useState<PlanNamespace.GET.PlansData['plans'][0]>()
   const [selectedDuration, setSelectedDuration] = useState<PlanNamespace.GET.PlansData['plans'][0]['durations'][0]>()
 
-  const { data: plansData, isLoading: isPlansLoading, error: plansError } = useSWRImmutable<PlanNamespace.GET.PlansData>(`${process.env.NEXT_PUBLIC_BACK_API_URL}/plans`)
+  const { isAuthenticated } = useUser()
+  const { data: plansData, isLoading: isPlansLoading, error: plansError } = useSWRImmutable<PlanNamespace.GET.PlansData>(isAuthenticated ? `${process.env.NEXT_PUBLIC_BACK_API_URL}/plans` : null)
 
   useEffect(() => {
     if (plansData) {
