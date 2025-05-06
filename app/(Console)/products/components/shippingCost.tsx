cddd78c711e74cf3585c4150af755be271ca6fe5
Sina Pirani <sinapiranidev@gmail.com>
2025-05-06 15:18:09 +0330

    Feat: Don't accept shipping Cost when is digital

--- app/(Console)/products/components/shippingCost.tsx
+++ app/(Console)/products/components/shippingCost.tsx
@@ -5,12 +5,23 @@ import { Input } from "@/components/theme/ui/input";
 import ErrorMessage from "@/components/ui/errorMessage";
 import { useSelectOnFocus } from "@/hooks/useSelectOnFocus";
 import { useTranslations } from "next-intl";
+import { useEffect } from "react";
 import { useFormContext } from "react-hook-form";
 
 export function ShippingCost() {
-  const { control } = useFormContext();
+  const { control, watch, setValue } = useFormContext();
   const t = useTranslations("Products.Form");
   const { onFocus } = useSelectOnFocus();
+
+  useEffect(() => {
+    if (watch('isDigital')) {
+      setValue('shippingCost', 0)
+    }
+  }, [watch('isDigital')])
+
+  if (watch('isDigital')) {
+    return null
+  }
 
   return (
     <div className="space-y-3 bg-blue-50/50 rounded-xl border border-blue-100 p-3 xl:p-5">
