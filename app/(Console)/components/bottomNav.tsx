92cac7843686ff40a81277786d5244e6f456fb92
Sina Pirani <sinapiranidev@gmail.com>
2025-05-25 17:57:25 +0330

    Debug: Add bottom margin to layout of console

--- app/(Console)/components/bottomNav.tsx
+++ app/(Console)/components/bottomNav.tsx
@@ -24,7 +24,7 @@ export function BottomNav({ items }: BottomNavbarProps) {
     useState<boolean>(false);
   const pathname = usePathname();
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50 bg-background border-t border-border md:hidden">    
+    <nav className="fixed h-[70px] bottom-0 left-0 right-0 z-50 bg-background border-t border-border md:hidden">    
           {
             isNavigationPending && (
               <AnimatedGradient
