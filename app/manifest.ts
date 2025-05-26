5f1197f4d5b66890a637f706eec7a6b7b5377e55
Sina Pirani <sinapiranidev@gmail.com>
2025-05-26 16:38:46 +0330

    Feat: Manifest

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'بفروش',
+    short_name: 'befroosh',
+    description: 'سیستم اتوماسیون فروش در شبکه‌های اجتماعی | دایرکت خودکار بفروش',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
