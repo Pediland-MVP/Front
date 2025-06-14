cb46214acc876da8e4c5fb757964eab69080ef94
Sina Pirani <sinapiranidev@gmail.com>
2025-06-14 14:39:08 +0330

    Debug: Forgeted change on showPopUp

--- app/(Console)/components/subscriptionExpireWarning.dialog.tsx
+++ app/(Console)/components/subscriptionExpireWarning.dialog.tsx
@@ -17,7 +17,7 @@ import useUser from "@/hooks/useUser"
 import ButtonLoading from "@/components/ui/button-loading"
 
 export default function SubscriptionExpireWarningDialog() {
-  const [showPopup, setShowPopup] = useState(true)
+  const [showPopup, setShowPopup] = useState(false)
   const [isAborted, setIsAborted] = useState(false)
   const [isNavigationLoading, setIsNavigationLoading] = useState(false)
   const [daysLeft, setDaysLeft] = useState<number | null>(null)
