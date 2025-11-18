4417a3ee35733f7f46cffa9e837992b7f2d0bcf7
Pedram Ghane <p.ghane@gmail.com>
2025-11-18 09:11:22 +0100

    feat: add favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,24 +2,26 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "بفروش",
-    short_name: "befroosh",
+    name: "Befroosh",
+    short_name: "Befroosh",
     description:
       "سیستم اتوماسیون فروش در شبکه‌های اجتماعی | دایرکت خودکار بفروش",
     start_url: "/",
     display: "standalone",
+    theme_color: "#ffffff",
     background_color: "#ffffff",
-    theme_color: "#000000",
     icons: [
       {
         src: "/favicon/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
         src: "/favicon/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
