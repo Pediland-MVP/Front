4134fc4f205d94cba898b184a5818dbf60f72c98
Sina Pirani <sinapiranidev@gmail.com>
2026-06-01 07:41:03 +0330

    Debug: Read next step from data.next

--- src/app/(Auth)/auth/page.tsx
+++ src/app/(Auth)/auth/page.tsx
@@ -64,7 +64,7 @@ export default function AuthPage() {
         },
       });
 
-      const next = response.data?.data?.result?.next;
+      const next = response.data?.data?.next;
 
       if (next === "password") {
         router.push(`/auth/password`);
